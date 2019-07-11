e0c13520fbf14334bc4f28667ac1dd5e5a36868c
timparker14 <tim.parker14@ntlworld.com>
2019-07-11 21:29:55 +0100

    Add files via upload
    updated so that incorrect selection doesn't bomb out. i.e. only exits on q.

--- shopping_list.py
+++ shopping_list.py
@@ -2,34 +2,38 @@ shopping_list = []
 new_item = str
 remove_item = str
 
-print("Hi, Let's sort your shopping list.")
+print("Hi, Let's work your shopping list.")
 
 while True:
 
     selection = input("What would you like to do?:\n"
-                  "A. View the list\n"
-                  "B. Add to list\n"
-                  "C. Remove an item from the list\n"
-                  "D. Write list to file\n"
-                  "Q. Exit\n >")
+                      "a. View the list\n"
+                      "b. Add to list\n"
+                      "c. Remove an item from the list\n"
+                      "d. Write list to file\n"
+                      "q. Exit\n >")
 
-    if selection == "A":
+    if selection == "a":
         print(shopping_list)
 
-    elif selection == "B":
+    elif selection == "b":
         new_item = input("What would you like to add?:\n >")
         shopping_list.append(new_item)
         print(shopping_list)
 
-    elif selection == "C":
-        remove_item = str(input("What would you like to add?:\n >"))
+    elif selection == "c":
+        remove_item = str(input("What would you like to remove?:\n >"))
+        print(shopping_list)
         shopping_list.remove(remove_item)
         print(shopping_list)
 
-    elif selection == "D":
-        with open('output.txt', 'w') as filehandle:
+    elif selection == "d":
+        with open('shopping.txt', 'w') as filehandle:
             filehandle.writelines("%s\n" % place for place in shopping_list)
 
-    else:
+    elif selection == "q":
+        print("Ok, bye. :)")
         break
-print("Ok, Bye :)")
+
+    else:
+        print("That's not one of the options. Try again numpty.")
